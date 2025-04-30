56d1cbd067d3fef7e1504c4b32f341470e4a14ad
SRI SASHANK POTLURU <srisashankpotluru@SRIs-MacBook-Pro.local>
2025-04-30 15:36:24 -0400

    Remove withNetlify from next.config.js for Netlify compatibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-const { withNetlify } = require('@netlify/next');
-module.exports = withNetlify();
+
 
 export default nextConfig;
